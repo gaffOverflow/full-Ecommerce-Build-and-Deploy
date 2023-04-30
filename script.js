7405eaf5a7b294bd4a72f05ee8d4cafc521b4fee
gaffOverflow <gaffar.ibrahim18@gmail.com>
2023-04-30 09:56:44 +0100

    new features added... product image toggle and style component applied

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
+// variable items
 const toggle = document.querySelector("#toggle");
 const toggleClose = document.querySelector("#toggleClose");
 const navItem = document.querySelector("#navItem");
 const link = document.querySelectorAll("a[href]");
 const nav = document.querySelector("nav");
+const mainImg = document.querySelector("#mainImg");
+const smallImg = document.querySelectorAll("#smallImg");
 
+// nav open toggle on click
 toggle.addEventListener("click", function () {
   // navItem.classList.add("duration-500");
   navItem.classList.remove("w-[0%]");
   navItem.classList.add("w-[60%]");
 });
+
+// nav close toggle
 toggleClose.addEventListener("click", function () {
   navItem.classList.remove("w-[60%]");
   navItem.classList.add("w-[0%]");
 });
+
+// toggle nav close on nav-links click
 link.forEach((x) =>
   x.addEventListener("click", function () {
     navItem.classList.remove("w-[60%]");
@@ -20,6 +28,8 @@ link.forEach((x) =>
     // navItem.style.width="0px"
   })
 );
+
+// nav slide up and down on windows scroll
 window.addEventListener("scroll", function () {
   if (document.documentElement.scrollTop > 250) {
     nav.classList.remove("top-0");
@@ -28,4 +38,11 @@ window.addEventListener("scroll", function () {
     nav.classList.remove("-top-20");
     nav.classList.add("top-0");
   }
+});
+
+// product img toggles
+smallImg.forEach((x) => {
+  x.addEventListener("click", function () {
+    mainImg.src = x.src;
+  });
 });
